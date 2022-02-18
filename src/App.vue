bbd2e6094479b4324e3a58a07a59c90f38682018
Andrey Poymanov <poym4nov@yandex.ru>
2022-02-18 14:04:54 +0300

    begin

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <hero-view></hero-view>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+import HeroView from "@/views/HeroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+export default {
+  components: { HeroView, FooterComponent },
+};
+</script>
+
+<style>
+</style>
