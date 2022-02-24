171ef0243dd7e5f40e69e09fa9ca2662fb51be2e
Andrey Poymanov <poym4nov@yandex.ru>
2022-02-24 22:46:58 +0300

    data component

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -77,5 +77,27 @@ import BestItemComponent from "@/components/BestItemComponent.vue";
 
 export default {
   components: { NavBarComponent, BestItemComponent },
+  data() {
+    return [
+      {
+        id: 0,
+        img: "coffee-1.jpg",
+        title: "Solimo Coffee Beans 2kg",
+        price: "10.73$",
+      },
+      {
+        id: 1,
+        img: "coffee-2.jpg",
+        title: "Presto Coffee Beans 1kg",
+        price: "15.99$",
+      },
+      {
+        id: 2,
+        img: "coffee-3.jpg",
+        title: "AROMISTICO Coffee 1kg",
+        price: "6.99$",
+      },
+    ];
+  },
 };
 </script>
