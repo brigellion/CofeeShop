357873e474234b9137395168583025ec99f4db3e
Andrey Poymanov <poym4nov@yandex.ru>
2022-03-17 17:58:22 +0300

    checkbox trouble

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -23,7 +23,7 @@
 
             <form @submit.prevent="submit" action="/" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
@@ -31,16 +31,19 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.name"
+                    v-model="v$.name.$model"
                     type="text"
                     class="form-control"
                     id="name-input"
                   />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
@@ -48,25 +51,31 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.email"
+                    v-model="v$.email.$model"
                     type="email"
                     class="form-control"
                     id="email-input"
                   />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.phone"
+                    v-model="v$.phone.$model"
                     type="tel"
                     class="form-control"
                     id="phone-input"
                   />
+                  <span v-for="error in v$.phone.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
@@ -79,13 +88,37 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
+                  <label for="checkbox" class="mb-0">
+                    Agree with the contract offer
+                    <span style="color: red">*</span>
+                  </label>
+                </div>
+                <div class="col col-12 col-sm-9">
+                  <input
+                    type="checkbox"
+                    checked
+                    id="checkbox"
+                    @change="changeCb"
+                    v-model="v$.checkbox.$model"
+                  />
+                  <span v-for="error in v$.checkbox.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
@@ -104,25 +137,55 @@
 
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
-import BestItemComponent from "@/components/BestItemComponent.vue";
+import useVuelidate from "@vuelidate/core";
+import { required, email, maxLength, sameAs } from "@vuelidate/validators";
+import { helpers } from "@vuelidate/validators";
+import { minLength } from "@/validators/minLength";
 
 export default {
-  components: { NavBarComponent, BestItemComponent },
+  components: { NavBarComponent },
+  setup() {
+    return { v$: useVuelidate() };
+  },
 
   data() {
     return {
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
-      },
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+      checkbox: "",
     };
   },
   methods: {
-    submit() {
-      console.log(this.form);
+    async submit() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) return;
+
+      console.log({
+        name: this.name,
+        email: this.email,
+        phone: this.phone,
+        message: this.message,
+        checkbox: this.checkbox,
+      });
     },
+    changeCb() {
+      this.checkbox = checkbox.checked;
+    },
+  },
+  validations() {
+    return {
+      name: { required },
+      email: { required, email },
+      phone: {},
+      message: {
+        required,
+        maxLength: maxLength(20),
+        minLength: helpers.withMessage("this value min 5", minLength),
+      },
+      checkbox: { sameAs: sameAs(() => true) },
+    };
   },
 };
 </script>
